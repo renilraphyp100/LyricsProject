a28017499c550a8d2ed6b2169c7c89dbeed9c0ab
Harsha MV <harsha@mink7.com>
2016-04-05 11:16:21 +0530

    Uploaded configuration

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,17 +1,18 @@
 class AlbumsController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :find_album, only: [:show, :edit, :update]
+
   def index
     @albums = Album.all
   end
-  
+
   def new
     @album = Album.new
   end
-  
+
   def create
     @album = Album.new(album_params)
- 
+
     if @album.save
       redirect_to @album
     else
@@ -19,30 +20,32 @@ class AlbumsController < ApplicationController
     end
 
   end
-  
+
   def show
-    @album = Album.find(params[:id])
   end
-  
+
   def edit
-    @album = Album.find(params[:id])
   end
-  
+
   def update
-    @album = Album.find(params[:id])
-   
+
     if @album.update(album_params)
       redirect_to @album
     else
       render 'edit'
     end
   end
-  
+
   private
-    def album_params
-      params.require(:album).permit(:name, :description, :picture, :artist_id)
-    end
-  
-  
-    
+
+  def find_album
+    @album = Album.find(params[:id])
+  end
+
+  def album_params
+    params.require(:album).permit(:name, :description, :picture, :artist_id)
+  end
+
+
+
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -15,8 +15,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.pluralize.parameterize}/#{model.id}"
   end
+
+  # process :optimize => [{ quality: 90 }]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -37,6 +39,13 @@ class PictureUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
   # end
+  version :small do
+    process :resize_to_fill => [48, 48]
+  end
+  
+  version :small1 do
+    process :resize_to_fit => [48, 48]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
@@ -49,5 +58,16 @@ class PictureUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  def filename
+    if original_filename
+      "#{model.name.parameterize}-#{secure_token(8)}.#{file.extension}"
+    end
+  end
+
+  protected
+  def secure_token(length=16)
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
+  end
 
 end

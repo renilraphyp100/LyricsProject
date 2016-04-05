8b3cf6b07f1bec03643286348cedd47cbd964eb0
Harsha MV <harsha@mink7.com>
2016-04-05 10:27:50 +0530

    Artist Validation for Description

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ActiveRecord::Base
     has_many :albums, dependent: :destroy
-    
+
     validates :name, presence: true, length: { minimum: 5 }
-    validates :description, presence: true
+    validates :description, presence: true, length: { minimum: 15 }
 end
